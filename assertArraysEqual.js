1afc7d817aa4e5d86b64c4c7a9c5d5ce0dc278d1
meriem farsi <farsi.meriem60@gmail.com>
2020-08-18 19:35:31 +0000

    assertArraysEqual

---
+++ assertArraysEqual.js
@@ -0,0 +1,25 @@
+// FUNCTION IMPLEMENTATION
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    return (console.log(`Assertion Passed: [${array1}] === [${array2}]`));
+  } else {
+    return (console.log(`Assertion failed: [${array1}] !== [${array2}]`));
+  }
+};
+const eqArrays = function(firstArray, secondArray) {
+//let result = true;
+  for (let i = 0; i < firstArray.length; i++) {
+    if (firstArray[i] !== secondArray[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+assertArraysEqual(["1","2","3"],["1","2","3"]);
+assertArraysEqual(["1","2","5"],["1","2","3"]);
+assertArraysEqual(["1","2","3"],["1","2"]);
+assertArraysEqual(["1","2","3"],[]);
+
+
+
