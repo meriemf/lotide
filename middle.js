4ffbd7f380e9d884cd84f53e528cf56fb44b7865
meriem farsi <farsi.meriem60@gmail.com>
2020-08-19 13:33:53 +0000

    middle.js function

---
+++ middle.js
@@ -0,0 +1,40 @@
+const eqArrays = function(firstArray, secondArray) {
+  //let result = true;
+  for (let i = 0; i < firstArray.length; i++) {
+    if (firstArray[i] !== secondArray[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    return (console.log(`Assertion Passed: [${array1}] === [${array2}]`));
+  } else {
+    return (console.log(`Assertion failed: [${array1}] !== [${array2}]`));
+  }
+};
+const middle = function(array) {
+  let result = [];
+  let index = 0;
+  let size = array.length;
+  if (size <= 2) {
+    result = [];
+  } else {
+    if (size % 2 === 0) { // even number [1, 2, 3, 4] -> [2,3]
+      index = (size / 2) - 1;
+      result[0] = array [index];
+      result[1] = (array [index + 1]);
+    } else { // odd number [1, 2, 3, 4, 5] -> [3]
+      index = Math.trunc(size / 2);
+      result[0] = array[index];
+    }
+  }
+  return (result);
+};
+assertArraysEqual(middle([1, 2, 3, 4]),[2,3]);
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]),[3, 4]); // => [3, 4]
+assertArraysEqual(middle([]),[]); // -> []
+assertArraysEqual(middle([1,2]),[]); // -> []
+assertArraysEqual(middle([1]),[]); // -> []
+assertArraysEqual(middle([1, 3, 4, 5, 6 ,7, 8, 9]),[5, 6]); // -> [5, 6]
