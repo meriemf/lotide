fa937ec365c96367ae6c2c3d1ce3029e9a5ea8fa
meriem farsi <farsi.meriem60@gmail.com>
2020-08-23 14:03:01 +0000

    refactor assertArraysEqual

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,3 +1,5 @@
+const eqArrays = require('./eqArrays');
+
 // FUNCTION IMPLEMENTATION
 const assertArraysEqual = function(array1, array2) {
   if (eqArrays(array1, array2)) {
@@ -6,26 +8,24 @@ const assertArraysEqual = function(array1, array2) {
     return (console.log(`Assertion failed: [${array1}] !== [${array2}]`));
   }
 };
-const eqArrays = function(firstArray, secondArray) {
-//let result = true;
-if (firstArray.length !== secondArray.length)
-{
-  return false;
-}
-else {
-  for (let i = 0; i < firstArray.length; i++) {
-    if (firstArray[i] !== secondArray[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-};
+// const eqArrays = function(firstArray, secondArray) {
+// if (firstArray.length !== secondArray.length)
+// {
+//   return false;
+// }
+// else {
+//   for (let i = 0; i < firstArray.length; i++) {
+//     if (firstArray[i] !== secondArray[i]) {
+//       return false;
+//     }
+//   }
+//   return true;
+// };
+// };
 
-assertArraysEqual(["1","2","3"],["1","2","3"]);
-assertArraysEqual(["1","2","5"],["1","2","3"]);
-assertArraysEqual(["1","2","3"],["1","2"]);
-assertArraysEqual(["1","2","3"],[]);
+module.exports = assertArraysEqual;
+
+
 
 
 
