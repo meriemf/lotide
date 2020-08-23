43b896f36f6b77d1bbf9f03092b65fc7eccdd135
meriem farsi <farsi.meriem60@gmail.com>
2020-08-23 05:46:51 +0000

    lotide refactor

--- head.js
+++ head.js
@@ -1,25 +1,20 @@
 // AssertEqual Function
 
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-  //return (console.log("Assertion Passed: ["+ actual + "] === [" + expected + "]"));
-    return (console.log(`Assertion Passed: [${actual}] === [${expected}]`));
-  } else {
-  // return (console.log("Assertion failed: ["+ actual + "] !== [" + expected + "]"));
-    return (console.log(`Assertion failed: [${actual}] !== [${expected}]`));
-  }
-};
+// const assertEqual = function(actual, expected) {
+//   if (actual === expected) {
+//   //return (console.log("Assertion Passed: ["+ actual + "] === [" + expected + "]"));
+//     return (console.log(`Assertion Passed: [${actual}] === [${expected}]`));
+//   } else {
+//   // return (console.log("Assertion failed: ["+ actual + "] !== [" + expected + "]"));
+//     return (console.log(`Assertion failed: [${actual}] !== [${expected}]`));
+//   }
+// };
 
 // Function Head
-
 const head = function(array) {
   return array[0];
 };
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head(["Lighthouse", "Labs"]), "Hello");
-assertEqual(5,head([5,6,7]));
-assertEqual(7,head([5,6,7]));
+module.exports = head;
 
 
 
