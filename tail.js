6e0874bc44083e72ff5c77ff8759a29e245dca53
meriem farsi <farsi.meriem60@gmail.com>
2020-08-23 05:53:04 +0000

    tail refactor

--- tail.js
+++ tail.js
@@ -11,22 +11,22 @@ const assertEqual = function(actual, expected) {
 };
 
 // Tail function
-const tail = function(array) {
+const tail = function(array){
   let newArray = array.slice(1,array.length);
-  return newArray;
+ // console.log(assertEqual(array.length,newArray.length));
+// compare strings
+//for (let i=0; i< newArray.length; i++)
+  //{
+    //assertEqual(newArray[i],"Labs");
+  //}
+ return newArray;
   
 };
-
+x = 12;
+if(true) {
+  x + 2;
+  x = 4;
+}
+//console.log(x);
+module.exports = tail;
 // test code
-console.log("Test a non empty array");
-console.log(tail(["Hello", "Lighthouse", "Labs"]));
-console.log("Test an empty array");
-console.log(tail([]));
-console.log("Test an array with one element");
-console.log(tail(['labs']));
-
-// Test Case: Check the original array length dosnt change
-console.log("Test case original array has the same length ");
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-console.log(tail(words)); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
