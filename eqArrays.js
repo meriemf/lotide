b7d5ed51753c4590b3bb62f21f6ac0ad2392535f
meriem farsi <farsi.meriem60@gmail.com>
2020-08-23 13:41:14 +0000

    refactor eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -1,13 +1,13 @@
-const assertEqual = function(actual, expected) {
-  const emojis = require('node-emoji');
-  if (actual === expected) {
-    //return (console.log("Assertion Passed: ["+ actual + "] === [" + expected + "]"));
-    return (console.log(`${emojis.random().emoji} Assertion Passed: [${actual}] === [${expected}]`));
-  } else {
-    // return (console.log("Assertion failed: ["+ actual + "] !== [" + expected + "]"));
-    return (console.log(`${emojis.random().emoji} Assertion failed: [${actual}] !== [${expected}]`));
-  }
-};
+// const assertEqual = function(actual, expected) {
+//   const emojis = require('node-emoji');
+//   if (actual === expected) {
+//     //return (console.log("Assertion Passed: ["+ actual + "] === [" + expected + "]"));
+//     return (console.log(`${emojis.random().emoji} Assertion Passed: [${actual}] === [${expected}]`));
+//   } else {
+//     // return (console.log("Assertion failed: ["+ actual + "] !== [" + expected + "]"));
+//     return (console.log(`${emojis.random().emoji} Assertion failed: [${actual}] !== [${expected}]`));
+//   }
+// };
 
 const eqArrays = function(firstArray, secondArray) {
 //let result = true;
@@ -25,11 +25,6 @@ else {
 }
 };  
 
-console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
-console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
-console.log(eqArrays(["1", "2", "3"], [])); // => false
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+module.exports = eqArrays;
 
 
