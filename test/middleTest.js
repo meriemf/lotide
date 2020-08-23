d9022386f1651b5106da40d3734a4abd71e2469c
meriem farsi <farsi.meriem60@gmail.com>
2020-08-23 14:11:44 +0000

    refactor middle

---
+++ test/middleTest.js
@@ -0,0 +1,9 @@
+const assertArraysEqual = require ('../assertArraysEqual');
+const middle = require ("../middle");
+
+assertArraysEqual(middle([1, 2, 3, 4]),[2,3]);
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]),[3, 4]); // => [3, 4]
+assertArraysEqual(middle([]),[]); // -> []
+assertArraysEqual(middle([1,2]),[]); // -> []
+assertArraysEqual(middle([1]),[]); // -> []
+assertArraysEqual(middle([1, 3, 4, 5, 6 ,7, 8, 9]),[5, 6]); // -> [5, 6]
